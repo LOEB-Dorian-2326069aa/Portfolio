885fef642a66524e774c0a8d143a5eb9a0288adb
Dorian <dorian.loeb@etu.univ-amu.fr>
2024-11-03 14:13:07 +0100

    modification du Portfolio

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Portfolio - Dorian Loeb</title>
+  <title>Portfolio</title>
   <link rel="icon" href="img/favicon_valanor.png">
   <meta name="description" content="Portfolio de Dorian Loeb">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
