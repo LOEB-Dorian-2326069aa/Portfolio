e7280ee4ce033559d944ed7a563144cbed10162a
Dorian <dorian.loeb@etu.univ-amu.fr>
2024-11-07 15:59:38 +0100

    modification du Portfolio

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.php',
+      template: './index.html',
     }),
     new CopyPlugin({
       patterns: [
